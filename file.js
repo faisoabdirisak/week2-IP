7f8d00d05b2eeb2ae8fbd45a9d9ec16cec92b771
faisoabdirisak <faisoabdirisak@gmail.com>
2021-11-14 14:27:04 +0300

    it will add arrays and variables

---
+++ file.js
@@ -0,0 +1,5 @@
+var CC, YY, MM, DD, d, dValue;
+var femaleNames = ["Akosua", "Adwoa","Abenaa", "Akua", "Yaa", "Afua", "Ama"];
+var maleNames = ["Kwasi", "Kwadwo","Kawabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
+var dayNames = ["Sunday", "monday","Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
